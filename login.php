719ac76edb3734006f1d0b9a7ce7a6b0d6597573
Hamiche Majors <69982392+Hamiche74@users.noreply.github.com>
2023-10-18 08:38:51 +0100

    Update login.php
    Dans cet exemple, nous utilisons MySQLi pour se connecter à la base de données, préparer et exécuter une requête, et vérifier le mot de passe haché à l'aide de la "fonction password_verify". Assurez-vous de remplacer les valeurs factices par les véritables informations de connexion à votre base de données et d'adapter cette logique à votre base de données réelle.

--- login.php
+++ login.php
@@ -3,16 +3,38 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // Validez l'adresse e-mail et le mot de passe ici
-    // Vous devrez interagir avec votre base de données pour vérifier les informations d'identification de l'utilisateur.
+    // Valider l'adresse e-mail et le mot de passe en comparaison avec la base de données (c'est un exemple fictif)
+    $db_host = 'localhost';
+    $db_user = 'votre_utilisateur_mysql';
+    $db_pass = 'votre_mot_de_passe_mysql';
+    $db_name = 'nom_de_votre_base_de_donnees';
 
-    if (/* Votre logique d'authentification réussie ici */) {
-        // Redirigez l'utilisateur vers la page d'accueil ou une autre page sécurisée
-        header("Location: accueil.php");
+    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+    if ($conn->connect_error) {
+        die("Erreur de connexion à la base de données : " . $conn->connect_error);
+    }
+
+    $sql = "SELECT ID_Utilisateur, Mot_De_Passe FROM Utilisateurs WHERE Adresse_Email = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($user_id, $hashed_password);
+    $stmt->fetch();
+
+    if ($stmt->num_rows === 1 && password_verify($password, $hashed_password)) {
+        // Authentification réussie
+        session_start();
+        $_SESSION['user_id'] = $user_id; // Vous pouvez stocker d'autres informations de session si nécessaire
+        header("Location: accueil.php"); // Redirigez l'utilisateur vers la page d'accueil
         exit;
     } else {
-        // Affichez un message d'erreur si l'authentification échoue
+        // Authentification échouée
         echo "Authentification échouée. Veuillez réessayer.";
     }
+
+    $stmt->close();
+    $conn->close();
 }
 ?>
