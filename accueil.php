f951f0b1c7e8fbbbde9833e6d074ba4e43cc317d
Hamiche Majors <69982392+Hamiche74@users.noreply.github.com>
2023-10-18 08:53:01 +0100

    Create accueil.php
    Page d'accueil simple pour notre boutique en ligne une fois que l'utilisateur est authentifié avec succès.

---
+++ accueil.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bienvenue dans notre boutique en ligne</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+        h1 {
+            color: #333;
+        }
+        p {
+            font-size: 18px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Bienvenue dans notre boutique en ligne</h1>
+        <p>
+            Cher client,
+            Bienvenue sur notre boutique en ligne. Vous pouvez explorer notre vaste gamme de produits, passer des commandes et profiter de nos offres spéciales.
+        </p>
+
+        <!-- Ajoutez ici le contenu de votre page d'accueil, par exemple, les produits en vedette, des promotions, etc. -->
+
+        <p>
+            N'hésitez pas à parcourir notre boutique et à nous contacter si vous avez des questions. Merci de faire confiance à notre boutique en ligne.
+        </p>
+
+        <a href="deconnexion.php">Déconnexion</a> <!-- Lien de déconnexion vers un fichier PHP de déconnexion -->
+    </div>
+</body>
+</html>
