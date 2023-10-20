5fec862ed97c7445406194760846bc118b5589cc
Hamiche Majors <69982392+Hamiche74@users.noreply.github.com>
2023-10-20 10:30:32 +0100

    Update produit.php
    Lorsque l'utilisateur effectue une action, par exemple en cliquant sur "Ajouter au panier" ou "Ajouter à la liste de souhaits", déclenchez une requête AJAX pour communiquer avec le serveur en arrière-plan. Cette requête envoie des données au serveur et reçoit une réponse sans recharger la page.

--- produit.php
+++ produit.php
@@ -16,6 +16,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <title><?php echo $produit['Nom']; ?></title>
     <!-- Styles CSS et JavaScript pour améliorer la convivialité de la page -->
+    <!-- ... Votre HTML précédent ... -->
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('form').on('submit', function(event) {
+            event.preventDefault(); // Empêche la soumission du formulaire par défaut
+
+            var action = $(this).find('input[name="action"]').val(); // Récupère l'action (ajouter au panier ou à la liste de souhaits)
+            var produitID = <?php echo $produit['ID']; ?>; // ID du produit
+
+            $.ajax({
+                type: 'POST',
+                url: 'ajax_actions.php', // Page PHP qui gère les actions AJAX
+                data: { action: action, produitID: produitID },
+                success: function(response) {
+                    // Traitez la réponse du serveur, par exemple, mettez à jour l'icône du panier dans l'interface utilisateur
+                }
+            });
+        });
+    });
+</script>
+
+<!-- ... Votre HTML précédent ... -->
+
 </head>
 <body>
     <!-- Affichez ici les détails du produit, y compris l'image, la description, le prix, les options de personnalisation, etc. -->
